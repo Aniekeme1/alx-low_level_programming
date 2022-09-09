fc738233d7aedb07f829522262b1b4a5536b159d
Aniekeme Umoren <aniekemeumoren@gmail.com>
2022-09-09 03:44:42 +0000

    I created and compiled my first C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *  function main begins the execution of the program
+ *  return 0 indicates that the programme is successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
