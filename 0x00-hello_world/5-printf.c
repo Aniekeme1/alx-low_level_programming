191a8d6f4dc147c1737ca93c869c175e40e81277
Aniekeme Umoren <aniekemeumoren@gmail.com>
2022-09-09 03:59:12 +0000

    Wrote another file of C code using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This is where the code begins to run and execute
+ * return - This is where the code will be successful if the return is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
